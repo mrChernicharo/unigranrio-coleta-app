534fe3b1a9d5e60350efd12b88c6f6e9b134aa27
Felipe Chernicharo <string7dev@gmail.com>
2022-04-19 19:18:07 -0400

    zoom, map & markers info

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import config from './_config.js';
-
+import { config, mapThemes } from './config.js';
+let initialZoom = 12;
+const points = [];
+let currentPoint = null;
+const pointInfoContainer = document.querySelector('#selected-point-info');
 const locations = [
 	{ name: 'Cruz Vermelha', lat: -22.9119, lng: -43.188182 },
 	{ name: 'Tavares Bastos', lat: -22.9265298, lng: -43.1798004 },
@@ -36,24 +39,52 @@ function initMap() {
 	const { lat, lng } = locations[0];
 
 	const map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 12,
+		zoom: initialZoom,
 		center: { lat, lng },
+		styles: mapThemes.retro,
 	});
 
-	setMarkers(map);
+	setPoints(map);
+
+	map.addListener('center_changed', () => {
+		console.log('center_changed', [map.center.lat(), map.center.lng()]);
+	});
 }
 
-function setMarkers(map) {
-	locations.forEach(loc => {
-		const { name, lat, lng } = loc;
+function setPoints(map) {
+	let i = 0;
+	locations.forEach(location => {
+		const { name, lat, lng } = location;
+
 		const marker = new google.maps.Marker({
 			position: { lat, lng },
+			map,
 			title: name,
-			// label: name,
 			icon: image,
+			// label: name,
+			// collisionBehavior:
+			// 	google.maps.CollisionBehavior.REQUIRED_AND_HIDES_OPTIONAL,
 		});
 
-		marker.setMap(map);
+		points.push({
+			title: marker.title,
+			latitude: lat,
+			longitude: lng,
+		});
+
+		marker.addListener('click', e => {
+			let zoom = map.zoom < 18 ? map.zoom + 1 : map.zoom;
+			map.setZoom(zoom);
+			map.setCenter();
+			currentPoint = points[i];
+			console.log('marker:click', {
+				e,
+				map,
+				marker,
+				markerData: points[i],
+			});
+			i++;
+		});
 	});
 }
 
